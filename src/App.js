84aa14a16f44584e22aba2e6a7f2e44287b82010
Mir Ahmed Raza <98475059+abusultan77@users.noreply.github.com>
2022-06-07 18:30:15 +0500

    first push

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <div className='container-fluid'>
+
+        <div className='row'>
+          <div className='col-sm-12 m-auto'>
+            <header className="App-header">
+
+              <a
+                className="App-link"
+                href="/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="\bacjdev.png"
+                  className="img-fluid width-100px"
+                  alt="Responsive image" />
+
+
+              </a>
+            </header>
+
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
